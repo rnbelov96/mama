56dc27f0aa9c8423f70957e85e175b802266f14c
Roman Belov <rnbelov96@gmail.com>
2021-08-26 16:15:25 +0300

    Настраивает закрытие модальных окон

--- src/ts/modals.ts
+++ src/ts/modals.ts
@@ -1,6 +1,8 @@
 /* eslint-disable no-param-reassign */
 export {};
 
+const openedModalList: Element[] = [];
+
 const modalFormInfoList = [
   {
     title: 'на бесплатную консультацию',
@@ -49,9 +51,8 @@ modalElList.forEach(modalEl => {
 const closeModalElList = document.querySelectorAll('.modal__close');
 closeModalElList.forEach(closeEl => {
   closeEl.addEventListener('click', () => {
-    modalElList.forEach(modalEL => {
-      closeModal(modalEL as HTMLDivElement);
-    });
+    closeModal(openedModalList[0] as HTMLDivElement);
+    openedModalList.shift();
   });
 });
 
@@ -59,6 +60,7 @@ closeModalElList.forEach(closeEl => {
 const policyBtnElList = document.querySelectorAll('.js-policy');
 policyBtnElList.forEach(el => {
   el.addEventListener('click', () => {
+    openedModalList.unshift(policyModalEl);
     openModal(policyModalEl as HTMLDivElement);
   });
 });
@@ -66,6 +68,7 @@ policyBtnElList.forEach(el => {
 const callbackBtnElList = document.querySelectorAll('.js-callback');
 callbackBtnElList.forEach(btn => {
   btn.addEventListener('click', () => {
+    openedModalList.unshift(formModalEl);
     formTitleEl.textContent = modalFormInfoList[0].title;
     formBtnEl.textContent = modalFormInfoList[0].button;
     openModal(formModalEl as HTMLDivElement);
@@ -75,6 +78,7 @@ callbackBtnElList.forEach(btn => {
 const presentBtnElList = document.querySelectorAll('.js-present');
 presentBtnElList.forEach(btn => {
   btn.addEventListener('click', () => {
+    openedModalList.unshift(formModalEl);
     formTitleEl.textContent = modalFormInfoList[1].title;
     formBtnEl.textContent = modalFormInfoList[1].button;
     openModal(formModalEl as HTMLDivElement);
@@ -84,6 +88,7 @@ presentBtnElList.forEach(btn => {
 const incomeBtnElList = document.querySelectorAll('.js-income');
 incomeBtnElList.forEach(btn => {
   btn.addEventListener('click', () => {
+    openedModalList.unshift(formModalEl);
     formTitleEl.textContent = modalFormInfoList[2].title;
     formBtnEl.textContent = modalFormInfoList[2].button;
     openModal(formModalEl as HTMLDivElement);
@@ -92,5 +97,6 @@ incomeBtnElList.forEach(btn => {
 
 const youtubeAdvBtnCallEl = document.querySelector('.js-youtube-adv');
 youtubeAdvBtnCallEl?.addEventListener('click', () => {
+  openedModalList.unshift(youtubeAdvModalEl);
   openModal(youtubeAdvModalEl as HTMLDivElement);
 });
